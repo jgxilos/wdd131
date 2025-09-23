c3646492cdd0fc2a487d258a621455029839fab5
jgxilos <jernigam.gonzalez@gmail.com>
2025-09-22 21:49:41 -0400

    W04 Learning Activity: Lazyloading Images

---
+++ week04/scripts/lazyload.js
@@ -0,0 +1,6 @@
+// Update the current year in the footer
+document.getElementById('currentYear').textContent = new Date().getFullYear();
+
+// Update the last modified date in the footer
+document.getElementById('lastModified').textContent = document.lastModified;
+
